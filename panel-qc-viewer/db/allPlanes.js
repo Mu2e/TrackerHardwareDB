253e806f0fa6169de2657353c51846c01f27d725
Andrew Edmonds <awje@bu.edu>
2023-06-06 16:06:02 -0500

    Update port and column name for allPlanes.js

--- panel-qc-viewer/db/allPlanes.js
+++ panel-qc-viewer/db/allPlanes.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 router
     .get('/', async(req, res, next) =>{
-    const response = await fetch('https://dbdata0vm.fnal.gov:8444/QE/mu2e/prod/app/SQ/query?dbname=mu2e_tracker_prd&t=qc.planes&o=plane&f=text');
+    const response = await fetch('https://dbdata0vm.fnal.gov:9443/QE/mu2e/prod/app/SQ/query?dbname=mu2e_tracker_prd&t=qc.planes&o=plane_id&f=text');
     const allPlaneInfo = await response.text();
 //	console.log(allPlaneInfo);
     return res.send(allPlaneInfo);
