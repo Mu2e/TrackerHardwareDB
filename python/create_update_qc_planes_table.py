b9d942844fef5cf385b9853f2b26c972c6117aa3
Andrew Edmonds <awje@bu.edu>
2023-06-06 15:17:38 -0500

    Add a python script for updating qc.planes table

---
+++ python/create_update_qc_planes_table.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+
+import time
+import numpy as np
+import pandas as pd
+
+parser = argparse.ArgumentParser(
+                    prog='create_update_qc_planes_table.py',
+                    description='Create SQL files for updating qc.planes table',
+                    epilog='For help contact Andy Edmonds via e-mail/Slack')
+
+parser.add_argument('--plane_id', required=True)
+parser.add_argument('--add_panels', nargs='*', help='Add these panels')
+parser.add_argument('--remove_panels', nargs='*', help='Remove these panels')
+
+args = parser.parse_args()
+dict_args = vars(args) # convert to dictionary
+
+plane_id=args.plane_id
+
+
+outfilename = '../sql/update_qc_planes_table.sql';
+print("Creating " + outfilename + " for plane number " + str(plane_id) + "...");
+
+update_sql_file = open(outfilename, 'w')
+if (dict_args['add_panels'] != None):
+    update_sql_file.write("UPDATE qc.planes SET panels=ARRAY_CAT(panels, \'{" + ', '.join(dict_args['add_panels']) + "}\') where plane=" + str(plane_id) + ";\n");
+if (dict_args['remove_panels'] != None):
+    # Have to remove elements one at a time in psql
+    for rem in dict_args['remove_panels']:
+        update_sql_file.write("UPDATE qc.planes SET panels=ARRAY_REMOVE(panels, "+rem+") where plane=" + str(plane_id) + ";\n");
+
+
+print("Done!");
+print("Now check " + outfilename + " looks OK and then run the following command:")
+print("  psql -h ifdb08 -p 5459 mu2e_tracker_prd < " + outfilename)
