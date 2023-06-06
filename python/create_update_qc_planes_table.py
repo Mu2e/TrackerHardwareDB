99bf733b8c048253252b7029308e468d83a7c1d1
Andrew Edmonds <awje@bu.edu>
2023-06-06 18:09:08 -0500

    Fix a column renaming issue

--- python/create_update_qc_planes_table.py
+++ python/create_update_qc_planes_table.py
@@ -25,7 +25,7 @@ print("Creating " + outfilename + " for plane number " + str(plane_id) + "...");
 
 update_sql_file = open(outfilename, 'w')
 if (dict_args['add_panels'] != None):
-    update_sql_file.write("UPDATE qc.planes SET panels=ARRAY_CAT(panel_ids, \'{" + ', '.join(dict_args['add_panels']) + "}\') where plane_id=" + str(plane_id) + ";\n");
+    update_sql_file.write("UPDATE qc.planes SET panel_ids=ARRAY_CAT(panel_ids, \'{" + ', '.join(dict_args['add_panels']) + "}\') where plane_id=" + str(plane_id) + ";\n");
 if (dict_args['remove_panels'] != None):
     # Have to remove elements one at a time in psql
     for rem in dict_args['remove_panels']:
