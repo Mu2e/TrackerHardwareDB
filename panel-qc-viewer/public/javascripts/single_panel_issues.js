d1e28ece7a02f96d2812800fd5b5777d96dc937a
Andrew Edmonds <aedmonds@york.cuny.edu>
2023-12-02 15:40:48 -0600

    Adding passes_flow_test and earflooding_trimming_donw columns to database, viewer, and scripts

--- panel-qc-viewer/public/javascripts/single_panel_issues.js
+++ panel-qc-viewer/public/javascripts/single_panel_issues.js
@@ -1,7 +1,7 @@
 export function single_panel_issues() {
-    return ["max_erf_fit", "hv_test_done", "earboard"]//, "passes_flow_test", "earflooding_trimming_done"]
+    return ["max_erf_fit", "hv_test_done", "earboard", "passes_flow_test", "earflooding_trimming_done"]
 }
 
 export function single_panel_issue_names() {
-    return ["Has Fe55 data?", "Has HV data?", "Passed earboard test?"]//, "passes_flow_test", "earflooding_trimming_done"]
+    return ["Has Fe55 data?", "Has HV data?", "Passed earboard test?", "Passes flow test?", "Earflooding trimming done?"]
 }
