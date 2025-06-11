289a8d7779eaf0a9418aa8392a1d3103bea54524
Andrew Edmonds <aedmonds@york.cuny.edu>
2025-06-11 10:03:29 -0500

    Adding dead channels plots to all_panel_qc

--- panel-qc-viewer/public/javascripts/all_panel_qc.js
+++ panel-qc-viewer/public/javascripts/all_panel_qc.js
@@ -1,7 +1,8 @@
-import { single_channel_issues } from './single_channel_issues.js'
+import { single_channel_issues, dead_channel_issues } from './single_channel_issues.js'
 import { single_panel_issues, single_panel_issue_names } from './single_panel_issues.js'
 import { get_panels_in_plane } from './get_panels_in_plane.js'
 const single_ch_issues = single_channel_issues();
+const dead_ch_issues = dead_channel_issues();
 const single_pan_issues = single_panel_issues();
 const single_pan_issue_names = single_panel_issue_names();
 
@@ -35,21 +36,25 @@ for (let i_panel = 0; i_panel < panels.length; i_panel++) {
     }
 }
 
+var n_dead_channels = Array(allPanelInfo.length); // keep track of total number of issues without double-counting
 for (let i_issue = 0; i_issue < single_ch_issues.length; ++i_issue) {
     var n_issue = Array(allPanelInfo.length);
     var issue = single_ch_issues[i_issue];
-//    console.log(issue)
+    //    console.log(issue)
     for (let i_panel = 0; i_panel < panels.length; i_panel++) {
+	if (i_issue == 0) {
+	    n_dead_channels[i_panel] = Array();
+	}
 	if (allPanelInfo[i_panel][issue] != null) {
 	    n_issue[i_panel] = allPanelInfo[i_panel][issue].length;
+	    if (dead_ch_issues.includes(issue)) {
+		n_dead_channels[i_panel] = n_dead_channels[i_panel].concat(allPanelInfo[i_panel][issue]); // this will contain all the channnel numbers (inc. duplicates)
+	    }
 	}
     }
 
     // Define Data
     var default_visible = '';
-    if (issue == "kapton_dots") { // at the moment, we don't want kapton_dots to count as an "issue" for the summary plot
-	default_visible = 'legendonly'; // so have it off by default, but still available if we want to know how many kapton_dots are on each panel
-    }
     single_channel_n_data[i_issue] = {name : issue,
 					  x: panels,
 					  y: n_issue,
@@ -58,10 +63,19 @@ for (let i_issue = 0; i_issue < single_ch_issues.length; ++i_issue) {
 				      visible:default_visible
 					 }
 }
-var single_channel_issue_vs_panel_plot = document.getElementById('single_channel_issue_vs_panel_plot');
+for (let i_panel = 0; i_panel < panels.length; i_panel++) {
+    n_dead_channels[i_panel] = [...new Set(n_dead_channels[i_panel])].length; // remove duplicate channel numbers
+}
+var dead_channels_data = {name : 'total',
+			  x: panels,
+			  y: n_dead_channels,
+			  mode:"markers",
+			  type:"bar",
+			 }
+
 var xaxis = {title : {text : 'panel number'}, tickmode : "linear", tick0 : 0.0, dtick : 10.0, gridwidth : 2};
-var yaxis = {title : {text : 'no. of channels with issues'}};
-var layout = { title : {text: "All Issues vs Panel Number"},
+var yaxis = {title : {text : 'no. of dead channels'}};
+var layout = { title : {text: "N Dead Channels vs Panel Number"},
 	       xaxis : xaxis,
 	       yaxis : yaxis,
 	       scroolZoom : true,
@@ -69,7 +83,19 @@ var layout = { title : {text: "All Issues vs Panel Number"},
 	       shapes: [ {type: 'line', x0: 0, y0: 5.0, x1: highest_panel_id, y1: 5.0, line:{ color: 'rgb(0, 0, 0)', width: 4, dash:'dot'} } ],
 	       colorway : colors
 	     };
+
+var dead_channels_vs_panel_plot = document.getElementById('dead_channels_vs_panel_plot');
+Plotly.newPlot(dead_channels_vs_panel_plot, [dead_channels_data], layout);
+
+var dead_channel_definition = document.getElementById('dead_channel_definition');
+dead_channel_definition.innerHTML = "Dead channel definition: "+dead_ch_issues.slice(0,7).join(", ") + "\n" + dead_ch_issues.slice(8).join(", ");
+
+var single_channel_issue_vs_panel_plot = document.getElementById('single_channel_issue_vs_panel_plot');
+layout.title.text = "Channel-level QC vs Panel Number";
+yaxis.title.text = 'no. of channels with issues';
+layout.shapes = Array();
 Plotly.newPlot(single_channel_issue_vs_panel_plot, single_channel_n_data, layout);
+
 
 for (let i_issue = 0; i_issue < single_pan_issues.length; ++i_issue) {
     var vals = Array(allPanelInfo.length);
@@ -154,23 +180,32 @@ for (let i_issue = 0; i_issue < single_ch_issues.length; ++i_issue) {
 	    const panel_info = allPanelInfo[panel_num_map.get(panel_number)];
 	    if (panel_info[issue] != null) {
 		n_issue[i_plane] += panel_info[issue].length;
-	    }
+	    }			
 	}
     }
 
     // Define Data
     var default_visible = '';
-    if (issue == "kapton_dots") { // at the moment, we don't want kapton_dots to count as an "issue" for the summary plot
-	default_visible = 'legendonly'; // so have it off by default, but still available if we want to know how many kapton_dots are on each panel
-    }
     single_channel_n_data_plane[i_issue] = {name : issue,
-					  x: planes,
-					  y: n_issue,
-					  mode:"markers",
+					    x: planes,
+					    y: n_issue,
+					    mode:"markers",
 					    type:"bar",
 					    visible:default_visible
-					 }
+					   }
 }
+
+// Count total dead channels
+var n_dead_channels_plane = Array(allPlaneInfo.length).fill(0);
+for (let i_plane = 0; i_plane < planes.length; i_plane++) {
+    planes[i_plane] = allPlaneInfo[i_plane]['plane_id'];
+    let panels = get_panels_in_plane(allPlaneInfo[i_plane])
+    for (let i_panel = 0; i_panel < panels.length; ++i_panel){
+	let panel_number = panels[i_panel];
+	n_dead_channels_plane[i_plane] += n_dead_channels[panel_num_map.get(panel_number)];
+    }
+}
+
 
 for (let i_issue = 0; i_issue < single_pan_issues.length; ++i_issue) {
     var passes = Array(allPanelInfo.length);
@@ -243,11 +278,17 @@ for (let i_issue = 0; i_issue < single_pan_issues.length; ++i_issue) {
 					   }
 }
 
+var dead_channels_plane_data = {name : 'total',
+				x: planes,
+				y: n_dead_channels_plane,
+				mode:"markers",
+				type:"bar",
+			       }
 
 var single_channel_issue_vs_plane_plot = document.getElementById('single_channel_issue_vs_plane_plot');
 var xaxis = {title : {text : 'plane number'}, tickmode : "linear", tick0 : 0.0, dtick : 1.0, gridwidth : 2};
-var yaxis = {title : {text : 'no. of channels with issues'}};
-var layout = { title : {text: "All Issues vs Plane Number"},
+var yaxis = {title : {text : 'no. of dead channels'}};
+var layout = { title : {text: "Total Dead Channels vs Plane Number"},
 	       xaxis : xaxis,
 	       yaxis : yaxis,
 	       scroolZoom : true,
@@ -255,6 +296,16 @@ var layout = { title : {text: "All Issues vs Plane Number"},
 	       shapes: [ {type: 'line', x0: 0, y0: 30.0, x1: planes.length, y1: 30.0, line:{ color: 'rgb(0, 0, 0)', width: 4, dash:'dot'} } ],
 	       colorway : colors
 	     };
+
+var dead_channels_vs_plane_plot = document.getElementById('dead_channels_vs_plane_plot');
+Plotly.newPlot(dead_channels_vs_plane_plot, [dead_channels_plane_data], layout);
+
+var dead_channel_definition_plane = document.getElementById('dead_channel_definition_plane');
+dead_channel_definition_plane.innerHTML = "Dead channel definition: "+dead_ch_issues.slice(0,7).join(", ") + "\n" + dead_ch_issues.slice(8).join(", ");
+
+layout.yaxis.title.text = "Total Channels";
+layout.title.text = "Single-channel QC vs Plane Number";
+layout.shapes = Array()
 Plotly.newPlot(single_channel_issue_vs_plane_plot, single_channel_n_data_plane, layout);
 
 var has_data_vs_plane_plot = document.getElementById('has_data_vs_plane_plot');
