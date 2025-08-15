fb39723fa3fc9991c82a6ab94ddb650b4c37ee9a
Andrew Edmonds <aedmonds@york.cuny.edu>
2025-08-15 15:50:00 -0500

    Fix ATD spreadsheet section of viewer

--- panel-qc-viewer/db/getPanelATDSpreadsheet.js
+++ panel-qc-viewer/db/getPanelATDSpreadsheet.js
@@ -4,8 +4,8 @@ const fetch = require('node-fetch');
 
 router
   .get('/:panel_id', async(req, res, next) =>{
-      const response = await fetch('https://dbdata0vm.fnal.gov:9443/QE/mu2e/prod/app/SQ/query?dbname=mu2e_tracker_prd&t=test.atd_spreadsheet&w=panel:eq:MN'+req.params.panel_id+"&f=text");
-//      const response = await fetch('https://dbdata0vm.fnal.gov:9443/QE/mu2e/prod/app/SQ/query?dbname=mu2e_tracker_prd&t=test.atd_spreadsheet&f=text');
+      const response = await fetch('https://dbdata0vm.fnal.gov:9443/QE/mu2e/prod/app/SQ/query?dbname=mu2e_tracker_prd&t=imported.atd_spreadsheet&w=panel:eq:MN'+req.params.panel_id+"&f=text");
+//      const response = await fetch('https://dbdata0vm.fnal.gov:9443/QE/mu2e/prod/app/SQ/query?dbname=mu2e_tracker_prd&t=imported.atd_spreadsheet&f=text');
 
     const repairsInfo = await response.text();
     return res.send(repairsInfo)
